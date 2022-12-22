0f5913306a840eb3dca8f9c0fbecb993c53ccce7
Daniel Copley <djcopley@users.noreply.github.com>
2022-12-21 23:57:02 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Daniel Copley on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
